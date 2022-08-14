7183b91f6ae1513a067febc89fc65f7cf312547e
andrewaclark248 <andrewaclark248@gmail.com>
2022-08-13 20:30:11 -0400

    validate user input

--- test_app.rb
+++ test_app.rb
@@ -1,4 +1,6 @@
 require "tty-prompt"
+require 'ostruct'
+
 prompt = TTY::Prompt.new
 
 def parse_input user_input
@@ -17,7 +19,7 @@ def parse_input user_input
         
         #if no operator skip else execute operator
         if current_operator
-            total = total.public_send(current_operator, current_integer) 
+            total = total.public_send(current_operator, current_integer.total) 
         else
             next
         end
@@ -32,7 +34,10 @@ def convert_fraction_to_integer user_input
         current_value = Rational(input).to_f 
         total = total + current_value
     end
-    return total
+    result = OpenStruct.new(success?: true, total: total)
+    return result
+rescue
+    return OpenStruct.new(success?: false)
 end
 
 def convert_integer_to_fraction integer
@@ -47,12 +52,27 @@ end
 
 def invalid_input user_input
     return true if !user_input
-    #split_input = user_input.split(" ")
+    valid = false
+    check_for_operator = false
+    split_input = user_input.split(" ")
+    split_input.each do |input|
+        if check_for_operator #check for existence of operator
+            if !["*", "/", "+", "-", "%"].include?(input)
+                valid = true
+                break
+            end
+        else #check for existence of integer/fraction
+            result = convert_fraction_to_integer(input)
+
+            if !result.success?
+                valid = true
+                break
+            end
+        end
+        check_for_operator = !check_for_operator
+    end
+    return valid
 end
-
-
-
-
 
 
 not_hit_exit = true
@@ -60,14 +80,16 @@ not_hit_exit = true
 while not_hit_exit
 
     user_input = prompt.ask("Enter alegrabic eqution (ex: 1+5) for awnser or type 'exit' to leave:")
-    
+
+    #exit cli if prompted
+    if user_input == "exit"
+        return
+    end
+
     #check for valid user input
     if invalid_input(user_input)
         puts "Invalid Input. Please enter value!"
         next
-    end
-    if user_input == "exit"
-        return
     end
 
     puts parse_input(user_input)
